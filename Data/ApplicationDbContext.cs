911da65c9ee2d6e5668a86531c2aa4d88eff2112
Mahmoud Hefny <mahmoud20-00025@student.eelu.edu.eg>
2023-12-24 22:16:39 +0200

    Done

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Coursaty.Models;
+
+namespace Coursaty.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Coursaty.Models.Course> Course { get; set; } = default!;
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,17 @@
+﻿namespace Coursaty.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int price { get; set; }
+
+        public Course() { 
+            
+        }
+    }
+}
