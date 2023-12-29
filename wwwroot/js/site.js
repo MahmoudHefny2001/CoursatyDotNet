da754f5275c9241dc50f4abaf7333ebae5dd0b56
MahmoudHefny2001 <hefny4@gmail.com>
2023-12-29 16:41:54 +0200

    Light theme functionality added

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.getElementById('theme-switcher').onclick = function () {
+    var themeStyle = document.getElementById('theme-style');
+
+    if (themeStyle.getAttribute('href') == '/css/light-theme.css') {
+        themeStyle.href = '/css/dark-theme.css';
+        localStorage.setItem('theme', 'dark');
+    } else {
+        themeStyle.href = '/css/light-theme.css';
+        localStorage.setItem('theme', 'light');
+    }
+};
+
+// On page load, set the theme from localStorage
+var savedTheme = localStorage.getItem('theme') || 'light';
+document.getElementById('theme-style').href = '/css/' + savedTheme + '-theme.css';
